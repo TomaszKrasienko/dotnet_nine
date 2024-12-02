79bddec63b7710aea37f54e69cc66e54e76aa68b
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-12-02 18:01:36 +0100

    Added neq linq methods

---
+++ linq_methods/linq_methods/Program.cs
@@ -0,0 +1,32 @@
+﻿//getting data
+
+using System.Text.Json;
+
+var stringData = await File.ReadAllTextAsync("data/linq_data_1.json");
+var persons = JsonSerializer.Deserialize<List<Person>>(stringData);
+
+var personsCount = persons?.CountBy(x => x.Job);
+foreach (var personCount in personsCount)
+{
+    Console.WriteLine($"{personCount.Key} : {personCount.Value}");
+}
+
+//results:
+//Frontend : 3
+//Backend : 2
+
+var personsAggregated = persons
+    .AggregateBy(keySelector: x => x.Job,
+        seed: 0m,
+        (totalValue, person) => totalValue + (person.Payment * person.Experience));
+
+foreach (var personAggregate in personsAggregated)
+{
+    Console.WriteLine($"{personAggregate.Key} : {personAggregate.Value}");
+}
+
+//results
+//Frontend : 31200
+// Backend : 61000
+
+public sealed record Person(int Id, string Name, string Job, int Payment, int Experience); 
