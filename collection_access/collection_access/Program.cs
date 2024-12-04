8e23b62752b3d5f8b040c33534cfda551757667f
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-12-04 21:24:32 +0100

    Added colleciton access

---
+++ collection_access/collection_access/Program.cs
@@ -0,0 +1,20 @@
+﻿var value = new[] {6, 9, 4, 20};
+
+var last = value[^1];
+var secondToLast = value[^2];
+
+var example = new Example()
+{
+    Array =
+    {
+        [^4] = 6,
+        [^3] = 9,
+        [^2] = 4,
+        [^1] = 20,
+    }
+};
+
+class Example
+{
+    public int[] Array = new int[5];
+}
